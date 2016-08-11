2aab2fefda4570e60cbb9a089074b3a3e85d6fcf
frcho <lgranados@kijho.com>
2016-08-11 14:33:16 -0500

    fix bugs and support symfony3 for choiceType::class in formType

--- Form/UserChatSettingsType.php
+++ Form/UserChatSettingsType.php
@@ -8,36 +8,64 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type as Type;
 
 class UserChatSettingsType extends AbstractType {
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options) {
-        $this->translator = $options['translator'];
+        $version = "3.0.0";
+        $symfonyVersion = \Symfony\Component\HttpKernel\Kernel::VERSION;
 
-        $builder
-                ->add('notificationSound', Type\ChoiceType::class, array(
-                    'required' => false,
-                    'label' => $this->translator->trans('admin_settings.notification_sound'),
-                    'choices' => array(
-                        'sounds-capisci.mp3' => 'Capisci',
-                        'sounds-come-to-daddy.mp3' => 'Come to Daddy',
-                        'sounds-communication-channel.mp3' => 'Communication Channel',
-                        'sounds-credulous.mp3' => 'Credulous',
-                        'sounds-et-voila.mp3' => 'Et Voila',
-                        'sounds-gets-in-the-way.mp3' => 'Gets in the way',
-                        'sounds-isnt-it.mp3' => "Isn't it",
-                        'sounds-no-way.mp3' => 'No way',
-                        'sounds-obey.mp3' => 'Obey',
-                        'sounds-pedantic.mp3' => 'Pedantic',
-                        'sounds-served.mp3' => 'Served',
-                        'sounds-surprise-on-a-spring.mp3' => 'Surprise',
-                        'sounds-worthwhile.mp3' => 'Worthwhile',
-                        'sounds-you-know.mp3' => 'You know',
-                        'sounds-your-turn.mp3' => 'Your turn',
-                    )
-                ))
-        ;
+        $this->translator = $options['translator'];
+        /**
+         * symfonyVersion  < version for symfony 2.8
+         */
+        if (version_compare($symfonyVersion, $version) == '-1') {
+            $builder->add('notificationSound', Type\ChoiceType::class, array(
+                'required' => false,
+                'label' => $this->translator->trans('admin_settings.notification_sound'),
+                'choices' => array(
+                    'sounds-capisci.mp3' => 'Capisci',
+                    'sounds-come-to-daddy.mp3' => 'Come to Daddy',
+                    'sounds-communication-channel.mp3' => 'Communication Channel',
+                    'sounds-credulous.mp3' => 'Credulous',
+                    'sounds-et-voila.mp3' => 'Et Voila',
+                    'sounds-gets-in-the-way.mp3' => 'Gets in the way',
+                    'sounds-isnt-it.mp3' => "Isn't it",
+                    'sounds-no-way.mp3' => 'No way',
+                    'sounds-obey.mp3' => 'Obey',
+                    'sounds-pedantic.mp3' => 'Pedantic',
+                    'sounds-served.mp3' => 'Served',
+                    'sounds-surprise-on-a-spring.mp3' => 'Surprise',
+                    'sounds-worthwhile.mp3' => 'Worthwhile',
+                    'sounds-you-know.mp3' => 'You know',
+                    'sounds-your-turn.mp3' => 'Your turn',
+                )
+            ));
+        } else {
+            $builder->add('notificationSound', Type\ChoiceType::class, array(
+                'required' => false,
+                'label' => $this->translator->trans('admin_settings.notification_sound'),
+                'choices' => array(
+                    'Capisci' => 'sounds-capisci.mp3',
+                    'Come to Daddy' => 'sounds-come-to-daddy.mp3',
+                    'Communication Channel' => 'sounds-communication-channel.mp3',
+                    'Credulous' => 'sounds-credulous.mp3',
+                    'Et Voila' => 'sounds-et-voila.mp3',
+                    'Gets in the way' => 'sounds-gets-in-the-way.mp3',
+                    "Isn't it" => 'sounds-isnt-it.mp3',
+                    'No way' => 'sounds-no-way.mp3',
+                    'Obey' => 'sounds-obey.mp3',
+                    'Pedantic' => 'sounds-pedantic.mp3',
+                    'Served' => 'sounds-served.mp3',
+                    'Surprise' => 'sounds-surprise-on-a-spring.mp3',
+                    'Worthwhile' => 'sounds-worthwhile.mp3',
+                    'You know' => 'sounds-you-know.mp3',
+                    'Your turn' => 'sounds-your-turn.mp3',
+                )
+            ));
+        }
     }
 
     /**
@@ -59,7 +87,7 @@ class UserChatSettingsType extends AbstractType {
     }
 
     public function getBlockPrefix() {
-        $this->getName();
+        return $this->getName();
     }
 
 }
