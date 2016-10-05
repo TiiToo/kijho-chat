6b4fcc6cc7cfa14c56c813e9dae5fd39c58ac5c8
frcho <lgranados@kijho.com>
2016-10-05 15:23:51 -0500

    command for run chat add merger options

--- EventListener/PidFileEventListener.php
+++ EventListener/PidFileEventListener.php
@@ -10,10 +10,12 @@ use Symfony\Component\Console\Input\ArgvInput;
 class PidFileEventListener {
 
     public function onConsoleCommand(ConsoleCommandEvent $event) {
-        
+
         $definition = $event->getCommand()->getDefinition();
         // add the option to the application's input definition
         $definition->addOption(new InputOption('pidfile', null, InputOption::VALUE_OPTIONAL, 'The location of the PID file that should be created for this process', null));
+        // merge the application's input definition
+        $event->getCommand()->mergeApplicationDefinition();
         // the input object will read the actual arguments from $_SERVER['argv']
         $input = new ArgvInput();
         // bind the application's input definition to it
@@ -24,7 +26,6 @@ class PidFileEventListener {
         if ($pidFile !== null) {
             file_put_contents($pidFile, getmypid());
         }
-        
     }
 
     public function onConsoleTerminate(ConsoleTerminateEvent $event) {
